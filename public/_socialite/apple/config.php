d0f6c88a6b1c3eb54843b35ce5255b7f7b172c77
Ahilanm <ahilanm@dckap.com>
2020-04-27 00:34:01 +0530

    removed unwanted files

--- public/_socialite/apple/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use \Apple\Apple;
-
-@session_start();
-
-Apple::setup('', '', '', '', 'https://xxx/callback.php', 'https://xxx/index.php');
