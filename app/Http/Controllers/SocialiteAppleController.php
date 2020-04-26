210b9e5b15777755aa102822ac87e6a937722555
Ahilanm <ahilanm@dckap.com>
2020-04-27 03:20:41 +0530

    composer changes

--- app/Http/Controllers/SocialiteAppleController.php
+++ app/Http/Controllers/SocialiteAppleController.php
@@ -15,7 +15,7 @@ class SocialiteAppleController extends Controller
     public function redirectToProvider($provider)
     {
         return Socialite::driver($provider)
-                        ->stateless()
+                        //->stateless()
                         ->redirect();
     }
 
@@ -27,16 +27,18 @@ class SocialiteAppleController extends Controller
     public function handleProviderCallback($provider)
     {
         $user = Socialite::driver($provider)
-                        ->stateless()
+                        //->stateless()
                         ->user();
-        dd($user);
-        return $user;
+        dump($user);
+        return 'callback output data';
     }
 
     public function tokenCallback($provider)
     {
         $token = 'eyJraWQiOiJlWGF1bm1MIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLnZvbmVjLnNpd2EuYXBpIiwiZXhwIjoxNTg3OTI2MjAzLCJpYXQiOjE1ODc5MjU2MDMsInN1YiI6IjAwMTcxMC44NThkN2NhNWUwZDg0MWI5ODFiNGVkYWY2NWM0M2ZmNi4xOTMyIiwiYXRfaGFzaCI6IjRHZFprR0k2X2Q3Qk5xMFFJTkhKZEEiLCJlbWFpbCI6ImFoaWxtdXJ1Z2VzYW5AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNTg3OTI1NjAxLCJub25jZV9zdXBwb3J0ZWQiOnRydWV9.ciXdwwkySnG-Ne_l9NqxuLkDPyptUVvJ_Puk10LSsXNEtLBAijskQhIjwi3HYsEXNLdlbMGfJ25rnlMWu93RoqYJFo_u_rFjH_4Xt9E_ddnqY147yZvVw5k912FtXabQSl2bFiR7yrzuQvznxyAiYFP9v9HvXyTcYS2ki6ISdPjmTyb927yWyGDx-aigksV752toAA8XXmjjEyi01eY-wng4CaV4mxjJU_bQSpnh6zGLpmI-lxqBIfSbvW1ukMDh9VW7fIRq9l3yFba91TAT9oBv7QQVcEAU7jHNzKX3qU7JvCfr7d2UUXFVkOxYZFz1HuPHB5C9QuYn5TtFUb2ozw';
-        dd(Socialite::driver($provider)->userFromToken($token));
+
+        dump(Socialite::driver($provider)->userFromToken($token));
+        return 'token output data';
         //$user;
     }
 }
--- config/services.php
+++ config/services.php
@@ -30,6 +30,17 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'apple' => [
+        "redirect" => env("APPLE_REDIRECT_URI"),
+        "key_id" => env("APPLE_KEY_ID"),
+        "team_id" => env("APPLE_TEAM_ID"),
+        "auth_key" => env("APPLE_AUTH_KEY"),
+        "client_id" => env("APPLE_CLIENT_ID"),
+        "client_secret" => env("APPLE_CLIENT_SECRET"),
+        "client_secret_updated_at" => env("APPLE_CLIENT_SECRET_UPDATED_AT"),
+        "refresh_token_interval_days" => env("APPLE_REFRESH_TOKEN_INTERVAL_DAYS"),
+    ],
+
     'twitter' => [
         'client_id' => env('TWITTER_KEY'),
         'client_secret' => env('TWITTER_SECRET'),
